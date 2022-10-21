fbf1cc9d65eb18e07c6da40844941962bcd6d282
Gabriel Boldi <gabriel.boldi@protonmail.com>
2022-10-21 00:19:58 -0300

    Estrutura dos elementos criado

--- js/main.js
+++ js/main.js
@@ -13,8 +13,11 @@ btnAdiciona.addEventListener("click", (event) => {
   event.preventDefault();
   const nome = document.querySelector("#novaTarefa");
 
+  !nome.value ? console.log("Tarefa em branco, ta de Parabéns") : console.log(`Tarefa: ${nome.value}`);
+
   const tarefa = {
     nome: nome.value,
+    estado: false,
   };
 
   tarefa.id = item[item.length - 1] ? item[item.length - 1].id + 1 : 0;
@@ -28,27 +31,6 @@ btnAdiciona.addEventListener("click", (event) => {
 });
 
 function criaElemento(elemento) {
-  // const li = document.createElement("li");
-  // li.classList.add("custom-checkbox");
-  
-  // const nome = document.createElement("p");
-  // nome.innerText = elemento.nome;
-
-  // const input = document.createElement("input");
-  // input.setAttribute("type", "checkbox");
-
-  // li.appendChild(input);
-  // li.appendChild(nome);
-
-  // li.appendChild(botaoEditar(elemento.id));
-  // li.appendChild(botaoDeleta(elemento.id));
-
-  // lista.appendChild(li);
-
-  lista.appendChild(criaElemento2(elemento));
-}
-
-function criaElemento2(elemento) {
   const li = document.createElement("li");
   li.classList.add("custom-checkbox");
 
@@ -56,10 +38,8 @@ function criaElemento2(elemento) {
   input.type = "checkbox";
   input.id = elemento.id;
 
-  const label = document.createElement("label");
-  label.setAttribute("for", elemento.id)
-  label.innerText = elemento.nome;
-  
+  const label = criaLabel(elemento, input);
+
   const div = document.createElement("div");
 
   const buttonEditar = document.createElement("button");
@@ -74,19 +54,50 @@ function criaElemento2(elemento) {
   div.appendChild(botaoDeleta(elemento.id));
   li.appendChild(div);
 
-  // lista.appendChild(li);
+  lista.appendChild(li);
+}
 
-  console.log(li);
-  return li;
+function criaLabel(elemento, input) {
+  var label = document.createElement("label");
+  label.setAttribute("for", elemento.id);
+  label.innerText = elemento.nome;
+
+  if (elemento.estado == true) {
+    input.checked = true;
+    label.classList.add("concluido");
+  } else {
+    input.checked = false;
+    label.classList.remove("concluido");
+  }
+
+  label.addEventListener("click", function () {
+    montaElemento(elemento, elemento.id);
+
+    let index = item.findIndex((element) => element.id === i_AUX);
+
+    if (elemento.estado == true) {
+      label.classList.remove("concluido");
+      item[index].estado = false;
+    } else {
+      label.classList.add("concluido");
+      item[index].estado = true;
+    }
+
+    localStorage.setItem("item", JSON.stringify(item));
+  });
+
+  return label;
+}
+
+function autoRefresh() {
+  window.location = window.location.href;
 }
 
 function botaoDeleta(id) {
   const buttonDelete = document.createElement("button");
-  // buttonDelete.innerText = "X";
   buttonDelete.classList.add("btnDeletar");
 
   buttonDelete.addEventListener("click", function () {
-    // console.log(this.parentNode.parentNode);
     deletaTarefa(this.parentNode.parentNode, id);
   });
 
@@ -106,7 +117,6 @@ function deletaTarefa(element, id) {
 
 function botaoEditar(id) {
   const btnEditar = document.createElement("button");
-  // btnEditar.innerText = "Editar";
   btnEditar.classList.add("btnEditar");
 
   btnEditar.addEventListener("click", function () {
@@ -127,13 +137,6 @@ function atualizar(elemento, id) {
   montaElemento(elemento, id);
 }
 
-var e_AUX;
-var i_AUX;
-function montaElemento(elemento, id) {
-  e_AUX = elemento;
-  i_AUX = id;
-}
-
 botaoConfirma.addEventListener("click", () => {
   e_AUX.querySelector("label").textContent = secEdicao.querySelector("input").value;
 
@@ -152,3 +155,10 @@ botaoCancela.addEventListener("click", () => {
 
   secEdicao.classList.add("invisivel");
 });
+
+var e_AUX;
+var i_AUX;
+function montaElemento(elemento, id) {
+  e_AUX = elemento;
+  i_AUX = id;
+}
